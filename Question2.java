424149efc0b43ce9e7b2c7be5d7cc2e7a671a576
yukieang <64241804+yukieang@users.noreply.github.com>
2020-04-25 15:12:21 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,6 +11,18 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    
+    /*System.out.print("Enter height in metres: ");*/
     double height = in.nextDouble();
+    
+    /*System.out.print("Enter weight in kg: ");*/
+    double weight = in.nextDouble();
+    
+    double BMI = weight / (height*height);
+    /*System.out.print("BMI is: " + BMI);*/
+    
+    
+    
+    
   }
 }
